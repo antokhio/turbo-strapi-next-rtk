935cdf9d34ee7cd313e293ae9bc97b1ff300ff8b
antokhio <antokhio@gmail.com>
2024-04-11 10:41:57 +0300

    added @repo/shared to backend

--- apps/_shared/src/types/shared.ts
+++ apps/_shared/src/types/shared.ts
@@ -1,3 +1,10 @@
 import { StrapiContentType } from './strapi';
 
 export type Layout = StrapiContentType<'api::layout.layout'>;
+
+export type Test = StrapiContentType<
+  'shared.seo',
+  {
+    someProp: string;
+  }
+>;
