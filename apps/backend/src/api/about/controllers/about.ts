6fbe668d74dcddc1602d5cd553ba2a69ec2bdf25
antokhio <antokhio@gmail.com>
2024-11-11 15:47:04 +0300

    checking precommit hook

--- apps/backend/src/api/about/controllers/about.ts
+++ apps/backend/src/api/about/controllers/about.ts
@@ -2,6 +2,18 @@
  * about controller
  */
 
-import { factories } from '@strapi/strapi'
+import type { Core } from "@strapi/strapi";
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::about.about');
+export default factories.createCoreController(
+  "api::about.about",
+  ({ strapi }: { strapi: Core.Strapi }) => ({
+    find: async () => {
+      const data = await strapi.documents("api::about.about").findFirst();
+
+      return {
+        data,
+      };
+    },
+  }),
+);
